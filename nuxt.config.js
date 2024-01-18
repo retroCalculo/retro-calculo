8971a834435e5b94e5b3236ebd7f8752e5a06c89
retroCalculo <157043699+retroCalculo@users.noreply.github.com>
2024-01-18 15:02:27 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'FrontendTaller',
+    title: 'Retro calculo',
     htmlAttrs: {
       lang: 'en'
     },
